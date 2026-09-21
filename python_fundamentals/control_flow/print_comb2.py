b7a9a1fc2c58aebf876831ce0ca5f29b93c97bc4
Nicky B <longsoakSTUDIOS@gmail.com>
2026-09-21 23:00:08 +1000

    removed 2nd while loop

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 i = 0
+j = 0
 while (i <= 9):
-    j = 0
-    while (j <= 9):
-        if j == 9 and i == 9:
-            print("{}{}".format(i, j))
+    if j == 9:
+        i += 1
+        j = 0
+    else:
         print("{}{}, ".format(i, j), end="")
         j += 1
-    i += 1
-
+        if j == 9 and i == 9:
+            print("{}{}".format(i, j))
+        
+     
 
