2b01e6e3e61658b3401be00147acb149502ec680
Nicky B <longsoakSTUDIOS@gmail.com>
2026-09-22 09:50:11 +1000

    replaced with for loop with end if condition

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,16 +1,4 @@
 #!/usr/bin/env python3
 
-i = 0
-j = 0
-while (i <= 9):
-    if j == 9:
-        i += 1
-        j = 0
-    else:
-        print("{}{}, ".format(i, j), end="")
-        j += 1
-        if j == 9 and i == 9:
-            print("{}{}".format(i, j))
-        
-     
-
+for i in range(0, 100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
