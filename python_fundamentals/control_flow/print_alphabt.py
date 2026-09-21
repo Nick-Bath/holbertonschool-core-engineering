66ea67172c6771b900b6186fc1aa3ba86490263a
Nicky B <longsoakSTUDIOS@gmail.com>
2026-09-21 22:39:34 +1000

    changed from fstring to string format

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-lowercase_alphabet = ''.join(chr(i) for i in range(ord('a'), ord('z') + 1))
 i = 0
 while i < 26:
     if i == 4 or i == 16:
         i += 1
-    print(f"{chr(ord('a') + i)}", end="")
+    print("{}".format(chr(ord('a') + i)), end="")
     i += 1
