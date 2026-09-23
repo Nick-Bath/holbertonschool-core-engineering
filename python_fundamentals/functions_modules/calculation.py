ab2821d06a0e1f980db0e8f04fe2281907ce99a7
Nicky B <longsoakSTUDIOS@gmail.com>
2026-09-23 21:48:44 +1000

    task 6

---
+++ python_fundamentals/functions_modules/calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import calculator_1
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{}".format(add(a,b)))
+    print("{}".format(sub(a,b)))
+    print("{}".format(mul(a,b)))
+    print("{}".format(div(a,b)))
+    
