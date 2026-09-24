fad57a199f47e79af2986dc1b60d1156619d5af8
Nicky B <longsoakSTUDIOS@gmail.com>
2026-09-25 07:46:25 +1000

    updated import to from __ import __

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import calculator_1 as calc
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{}".format(calc.add(a,b)))
-    print("{}".format(calc.sub(a,b)))
-    print("{}".format(calc.mul(a,b)))
-    print("{}".format(calc.div(a,b)))
+    print("{}".format(add(a,b)))
+    print("{}".format(sub(a,b)))
+    print("{}".format(mul(a,b)))
+    print("{}".format(div(a,b)))
